2ce6ce167fce4281c5556a547c4b649b35ef806f
karen <karen.chobanyan@gmail.com>
2017-11-28 18:26:28 +0400

    React 16

---
+++ Tracker/app/screens/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, Text, View } from 'react-native';
+import { colors } from '../config/styles';
+import Button from '../components/Button';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.background,
+  },
+  main: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: colors.headerText,
+    fontWeight: '400',
+    fontStyle: 'italic',
+  },
+});
+
+const Home = (props) => {
+  console.log('aaaaaaaa');
+  return (
+    <View style={styles.container}>
+      <Text style={styles.main}>
+        Home
+      </Text>
+      <Button
+        text="Details"
+        onPress={() => props.navigation.navigate('Details')}
+      />
+    </View>
+  );
+};
+
+Home.propTypes = {
+  navigation: PropTypes.object,
+};
+
+export default Home;
